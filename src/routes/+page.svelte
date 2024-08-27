708bad84a79e78812a3b80c1a8666d61cf88b54b
leonardotavares20 <leo.tav.12345@gmail.com>
2024-08-27 04:37:56 -0300

    init: test spline with sveltekit-app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Application } from "@splinetool/runtime";
+  import { onMount } from "svelte";
+
+  let canvas;
+  onMount(() => {
+    canvas = document.getElementById("canvas3d");
+    const app = new Application(canvas);
+    app.load("https://prod.spline.design/yBWnEoywvSzXwkna/scene.splinecode");
+  });
+</script>
+
+<canvas id="canvas3d"></canvas>
+
+<svelte:head>
+  <script
+    async
+    src="https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js"
+  ></script>
+</svelte:head>
